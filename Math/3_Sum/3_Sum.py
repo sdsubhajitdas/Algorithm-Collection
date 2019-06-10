41f6b156434fdf1e4dc86cee31b53adba2751abb
Subhajit Das <jeetudas95@gmail.com>
2019-06-11 01:19:12 +0530

    3_Sum updated

--- Math/3_Sum/3_Sum.py
+++ Math/3_Sum/3_Sum.py
@@ -5,7 +5,7 @@ def sum3_n2(array: list, s: int) -> tuple:
     for i in range(length-2):
         l = i+1
         r = length - 1
-        while(l<r):
+        while(l < r):
             if (array[i]+array[l]+array[r] == s):
                 return array[i], array[l], array[r]
             elif (array[i]+array[l]+array[r] < s):
@@ -14,28 +14,26 @@ def sum3_n2(array: list, s: int) -> tuple:
                 r -= 1
     return None
 
+
 def sum3_n(array: list, s: int) -> tuple:
     length = len(array)
 
     for i in range(length):
         table = dict()
         cur_s = s - array[i]
-        for j in range(i+1,length):
-            if(table.get(cur_s - array[j],-1) == True):
+        for j in range(i+1, length):
+            if(table.get(cur_s - array[j], -1) == True):
                 return array[i], cur_s - array[j], array[j]
             table[array[j]] = True
     return None
 
 
-def main():
+if __name__ == "__main__":
     array = [1, 4, 45, 6, 10, 8]
     s = 22
 
     # sum3_n2() function is an O(n^2) approach towards the problem
-    print(sum3_n2(array,s))
+    print(sum3_n2(array, s))
 
-    #sum_n() function uses a hashing table 
-    print(sum3_n(array,s))
-
-if __name__ == "__main__":
-    main()
+    # sum_n() function uses a hashing table
+    print(sum3_n(array, s))
